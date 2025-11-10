9a8b317a0dd7b54975638f4e76eb28a3f187355c
thanhthinh113 <146800825+thanhthinh113@users.noreply.github.com>
2025-11-11 02:09:20 +0700

    feat(deploy): chạy local

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -23,7 +23,9 @@ const StoreContextProvider = ({ children }) => {
   const [selectedCategory, setSelectedCategory] = useState("all");
   const [notifications, setNotifications] = useState([]);
 
-  const url = "https://backend-foodrestaurant.onrender.com";
+  //const url = "https://backend-foodrestaurant.onrender.com";
+  const url = "http://localhost:4000";
+
   const url_AI = "https://food-del-ai.onrender.com";
 
   // StoreContext
